ae58f012439ce1987e6de92805f4ec8f24a75fe3
Lucas Milhão <lucasmilhao683@gmail.com>
2026-01-21 01:51:43 -0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import Visao.JanelaPrincipal;
+import java.awt.EventQueue;
+
+public class App {
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JanelaPrincipal().setVisible(true);
+            }
+        });   
+    }
+}
